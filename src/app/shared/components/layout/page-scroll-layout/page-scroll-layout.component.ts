7eb95d45bf5aa99b952f8ea261f24e74accc6dfb
Vishnu Somanath <vishnu.somanath@speridian.com>
2019-12-27 12:20:41 +0530

    updates

--- src/app/shared/components/layout/page-scroll-layout/page-scroll-layout.component.ts
+++ src/app/shared/components/layout/page-scroll-layout/page-scroll-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-page-scroll-layout',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageScrollLayoutComponent implements OnInit {
 
+  @Input() gutter;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  /* Public Methods */
+  setStyles() {
+    if (!this.gutter) {
+      this.gutter = 52;
+    }
+    let styles = {
+      'max-height': `calc(100% - ${this.gutter}px)`,
+      'margin-bottom': `-${this.gutter}px`
+    };
+    return styles;
   }
 
 }
